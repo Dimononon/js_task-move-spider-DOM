e6e6a3824c5c311cfc412e79ee7c4e8be93d09ae
Dimononon <dimononk@gmail.com>
2026-03-04 20:22:16 +0200

    task complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,37 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  // write code here
+  if (!e.target.classList.contains('wall')) {
+    return;
+  }
+
+  const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
+
+  const [wallLeft, wallTop] = [wall.offsetLeft, wall.offsetTop];
+  const [wallWidth, wallHeight] = [wall.offsetWidth, wall.offsetHeight];
+  const [wallBorderLeft, wallBorderTop] = [wall.clientLeft, wall.clientTop];
+  const [spiderWidth, spiderHeight] = [spider.offsetWidth, spider.offsetHeight];
+
+  let newX = e.clientX - wallLeft - wallBorderLeft - spiderWidth / 2;
+  let newY = e.clientY - wallTop - wallBorderTop - spiderHeight / 2;
+
+  if (newX < 0) {
+    newX = 0;
+  }
+
+  if (newY < 0) {
+    newY = 0;
+  }
+
+  if (newX > wallWidth - spiderWidth - wallBorderLeft - wallBorderTop) {
+    newX = wallWidth - spiderWidth - wallBorderLeft - wallBorderTop;
+  }
+
+  if (newY > wallHeight - spiderHeight - wallBorderLeft - wallBorderTop) {
+    newY = wallHeight - spiderHeight - wallBorderLeft - wallBorderTop;
+  }
+
+  spider.style.left = `${newX}px`;
+  spider.style.top = `${newY}px`;
 });
